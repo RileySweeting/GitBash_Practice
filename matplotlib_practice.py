5114013fedba189ed16e30c5d376830edd61ab4e
Riley Sweeting <rileyts2004@gmail.com>
2024-05-21 13:29:05 -0400

    Initial commit - uploading all files to repository

---
+++ matplotlib_practice.py
@@ -0,0 +1,8 @@
+# Author: Riley Sweeting
+# Description: This file is used for practicing my Matplotlib skills, and contains example plots and visuals for future reference.
+
+# Import NumPy & MatPlotLib packages
+import numpy as np
+import matplotlib as mlp
+import matplotlib.pyplot as plt
+
